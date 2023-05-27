474907b42c6a1a5b12acf595f9a01b98295cc70b
vasiliigogolev <vasiliigogolev92@gmail.com>
2023-05-28 01:31:35 +0700

    readjust timeout

--- app/ai.py
+++ app/ai.py
@@ -4,12 +4,14 @@ from os import getenv
 openai.api_key = getenv("OPENAI_KEY")
 
 def generate(prompt:str) -> str:
+    print('generating prompt...')
     _messages = [
         {"role": "system", "content": getenv("SYSTEM_PROMPT")},
         {"role": "user", "content": prompt}
     ]
     try:
-        chat_completion = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=_messages, request_timeout=10.0)
+        chat_completion = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=_messages, request_timeout=9.0)
     except:
+        print('error generating prompt')
         return None
     return chat_completion.choices[0].message.content
