f99b29b882919e5b5368def2b8d4a161523e61fc
Vasilii Gogolev <vasiliy.gogolev@mytona.com>
2023-10-31 12:56:49 +0700

    fix index.py

--- api/index.py
+++ api/index.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
-from app.bot import processInput
+from app.bot import process_input
 import asyncio
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["POST"])
 def main():
     value = request.get_json()
-    asyncio.run(processInput(value))
+    asyncio.run(process_input(value))
     return jsonify({"status": "ok"})
+
 
 @app.route("/")
 def home():
